37ce1cfba0d6f9fb7b20b1fbf2b8549fe129da1f
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2023-12-19 23:46:04 +0800

    fix(contact): fallback for orderDirection

--- app/Scopes/Contact/ContactScope.php
+++ app/Scopes/Contact/ContactScope.php
@@ -4,28 +4,28 @@ namespace App\Scopes\Contact;
 
 trait ContactScope
 {
-    public function scopeOrderByWithDirection($query, string $orderBy, string $orderDirection)
+    public function scopeOrderByWithDirection($query, ?string $orderBy, ?string $orderDirection)
     {
         return $query
-            ->when($orderBy, function ($q) use ($orderBy, $orderDirection)
+            ->when(($orderBy && isset($orderBy)), function ($q) use ($orderBy, $orderDirection)
             {
-                $q->orderBy($orderBy, $orderDirection);
+                $q->orderBy($orderBy, $orderDirection ?? 'asc');
             });
     }
 
-    public function scopeLatestByID($query, int $latest)
+    public function scopeLatestByID($query, ?int $latest)
     {
         return $query
-            ->when($latest, function ($q)
+            ->when(($latest && isset($latest)), function ($q)
             {
                 $q->latest('id');
             });
     }
 
-    public function scopeSearchGenderEmail($query, string $search)
+    public function scopeSearchGenderEmail($query, ?string $search)
     {
         return $query
-            ->when($search, function ($q) use ($search)
+            ->when(($search && isset($search)), function ($q) use ($search)
             {
                 $q->where('email', 'like', '%' . $search . '%')
                     ->orWhere('gender', $search);
