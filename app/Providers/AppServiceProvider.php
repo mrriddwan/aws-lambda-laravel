58246fd32a2a17b3f56c92880051e338ab2dac64
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2023-12-20 17:35:26 +0800

    feat(deploy): using planet scale api call success

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $cacheDirectory = '/tmp/framework/cache';
+        $migrationsDirectory = '/tmp/database/migrations';
+
+        if (!file_exists($cacheDirectory))
+        {
+            mkdir($cacheDirectory, 0755, true);
+        }
+
+        if (!file_exists($migrationsDirectory))
+        {
+            mkdir($migrationsDirectory, 0755, true);
+        }
     }
 }
