1743145988b05a1a263393743fa72b44ad11b6c5
Amir Ridwan <83332805+mrriddwan@users.noreply.github.com>
2023-12-20 09:30:10 +0800

    fix(): redeploy for lambda and provide api documentation

--- config/database.php
+++ config/database.php
@@ -93,6 +93,26 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'vimigo_aws' => [
+            'driver' => 'mysql',
+            'url' => env('VIMIGO_AWS_DATABASE_URL'),
+            'host' => env('VIMIGO_AWS_DB_HOST', '127.0.0.1'),
+            'port' => env('VIMIGO_AWS_DB_PORT', '3306'),
+            'database' => env('VIMIGO_AWS_DB_DATABASE', 'forge'),
+            'username' => env('VIMIGO_AWS_DB_USERNAME', 'forge'),
+            'password' => env('VIMIGO_AWS_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
     ],
 
     /*
